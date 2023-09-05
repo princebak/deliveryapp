fe5c141ad3e22919694799dd0a067e282f19e827
princebak <princebakenga09032017@gamil.com>
2023-09-05 16:55:08 +0100

    update login url reset to prod

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -20,7 +20,7 @@ const handler = NextAuth({
         let user = undefined;
 
         const response = await fetch(
-          "http://localhost:3000/api/auth/login", //"https://deliveryapp-three.vercel.app/api/auth/login",
+          "https://deliveryapp-three.vercel.app/api/auth/login", //"http://localhost:3000/api/auth/login", //
           {
             method: "POST",
             headers: {
